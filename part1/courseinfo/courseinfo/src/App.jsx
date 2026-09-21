c6cebf3c85ea18418d1d677654f3ee52934d0375
Vinicius Quadrado <viniciusquadrado@hotmail.com>
2026-09-21 17:13:17 -0300

    Add initial project setup for React and Vite
    - Created essential project files including package.json, .gitignore, and README.md.
    - Configured ESLint with recommended settings for React.
    - Added Vite configuration for building and serving the application.
    - Implemented basic React components for course information display.
    - Included favicon and SVG icons for UI elements.

---
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+    </div>
+  )
+}
+
+export default App
